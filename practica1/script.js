703d8c3b422fb742c373bd00a836d500ff4e0d5d
Carlos Rico <ricgarcar@gmail.com>
2023-07-24 18:39:29 +0200

    Realizamos do while para meter notas alumnos

--- practica1/script.js
+++ practica1/script.js
@@ -1,1 +1,32 @@
-console.log("Hola mundo")
+    
+    const salir = ""
+    
+    do{
+        const nota = prompt("Introduce tu nota, pon un punto para salir ") 
+        if (nota >= 0 && nota <=2.99){
+            alert("Muy deficiente") 
+        }
+        
+        else if (nota >=3 && nota <=4.99){
+            alert("Insuficiente")
+        }
+        else if (nota >=5 && nota <=5.99){
+            alert("Suficiente")
+
+        }
+        else if (nota >=6 && nota <=6.99){
+            alert("Bien")
+            
+        }
+        else if (nota >=7 && nota <=8.99){
+            alert("Notable")
+        
+        }
+        else if (nota >=9 && nota <=10){
+            alert("Sobresaliente")
+            
+        }
+        
+    }while (nota != undefined)
+
+
